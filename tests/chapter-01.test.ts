a7ea14e32c10d326fef007c49544cdbc40d0ad66
Eric Rovell <ericrovell@gmail.com>
2021-09-02 23:45:53 +0300

    implement tests for number of days routine

--- tests/chapter-01.test.ts
+++ tests/chapter-01.test.ts
@@ -1,4 +1,5 @@
 import { calcEaster } from "@/chapters/01-time/the-date-of-easter";
+import { convertDateToDayNumber } from "@/chapters/01-time/converting-the-date-to-the-day-number";
 
 describe("Chapter 01: Time", () => {
 	it("Calculates the date of Easter", () => {
@@ -7,5 +8,13 @@ describe("Chapter 01: Time", () => {
 		expect(calcEaster(2021)).toEqual({ month: 4, day: 4 });
 		expect(calcEaster(2027)).toEqual({ month: 3, day: 28 });
 		expect(calcEaster(2041)).toEqual({ month: 4, day: 21 });
+	});
+	it("Calculates the number of days for the date", () => {
+		expect(convertDateToDayNumber({ year: 1990, month: 11 })).toBe(304);
+		expect(convertDateToDayNumber({ year: 2000, month: 5 })).toBe(121);
+		expect(convertDateToDayNumber({ year: 2019, month: 1 })).toBe(0);
+		expect(convertDateToDayNumber({ year: 2016, month: 1 })).toBe(0);
+		expect(convertDateToDayNumber({ year: 2027, month: 9 })).toBe(243);
+		expect(convertDateToDayNumber({ year: 2028, month: 9 })).toBe(244);
 	});
 });
