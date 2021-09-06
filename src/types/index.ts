f674b2a0f061f54b61ea448a3c1b0fc9dfe2cfad
Eric Rovell <ericrovell@gmail.com>
2021-09-06 12:07:43 +0300

    more types and better types composition

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,26 @@
 export interface Date {
 	year: number;
-	month?: number;
-	day?: number;
+	month: number;
+	day: number;
+}
+
+export interface Time {
+	hours: number;
+	minutes: number;
+	seconds: number;
+}
+
+/**
+ * Defines a Universal Time (UT)
+ */
+export interface UT extends Date, Time {}
+
+/**
+ * Defines a Local Civil Time.
+ * 
+ * Note: defined date is local date.
+ */
+export interface LCT extends UT {
+	daylightSaving: number;
+	zoneCorrection: number;
 }
