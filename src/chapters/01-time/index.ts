5dc1f940e96cc47d14dbc23a0f2a92de1b721298
Eric Rovell <ericrovell@gmail.com>
2021-09-27 13:16:01 +0300

    rename filenames to improve project naming conventions

--- src/chapters/01-time/index.ts
+++ src/chapters/01-time/index.ts
@@ -1,8 +1,8 @@
 export { convertDateToDayNumber } from "./convert-date-to-day-number";
 export { convertTimeToDecimalHours, convertDecimalHoursToTime } from "./convert-time-and-decimal-hours";
-export { getDayWeekName } from "./get-name-of-the-day-of-the-week";
-export { convertJulianToGreenwichDate, convertGreenwichToJulianDate } from "./julian-to-greenwich-date";
-export { calcEaster } from "./the-date-of-easter";
+export { getDayWeekName } from "./get-day-week-name";
+export { convertJulianToGreenwichDate, convertGreenwichToJulianDate } from "./convert-jd-to-gd";
+export { calcEaster } from "./calc-easter";
 export { convertLCTToUT } from "./convert-lct-to-ut";
 export { convertUTtoGST, convertGSTtoUT, checkGSTtoUT } from "./convert-ut-to-gst";
 export { convertGSTtoLST, convertLSTtoGST } from "./convert-gst-to-lst";
